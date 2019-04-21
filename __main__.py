10d3ab23433131a8b7f7ec60893c993bc6e40328
arhansolo <arhansolo@yandex.ru>
2019-04-21 21:52:28 +0500

    Hi

--- __main__.py
+++ __main__.py
@@ -1,30 +1,32 @@
-import telebot
-import apiai, json
-import time
 import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import apiai, json
 TOKEN = os.environ["TOKEN"]
-bot = telebot.TeleBot(TOKEN)
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Приветствую тебя, странник!")
-
-
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    API_TOKEN = os.environ["API_TOKEN"]
+API_TOKEN = os.environ["API_TOKEN"]
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
+def textMessage(bot, update):
     request = apiai.ApiAI(API_TOKEN).text_request()
     request.lang = 'ru'
-    request.session_id = "RUPB_bot"
-    request.query = bot.message_handler(message.text)
+    request.session_id = 'RUPB_bot'
+    request.query = update.message.text
     responseJson = json.loads(request.getresponse().read().decode('utf-8'))
     response = responseJson['result']['fulfillment']['speech']
+
     if response:
         bot.send_message(chat_id=update.message.chat_id, text=response)
     else:
         bot.send_message(chat_id=update.message.chat_id, text='Я Вас не совсем понял!')
 
+start_command_handler = CommandHandler('start', startCommand)
+text_message_handler = MessageHandler(Filters.text, textMessage)
 
+dispatcher.add_handler(start_command_handler)
+dispatcher.add_handler(text_message_handler)
 
+updater.start_polling(clean=True)
 
-bot.polling(none_stop=True)
+updater.idle()
+
