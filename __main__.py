234ed12964c18868784df6a9796cb627acd97295
arhansolo <arhansolo@yandex.ru>
2019-04-25 21:54:08 +0500

    break

--- __main__.py
+++ __main__.py
@@ -35,13 +35,15 @@ def send_gif(bot, update):
     keyboard = []
     keyboard.append([KeyboardButton(text="Да")])
     keyboard.append([KeyboardButton(text="Нет")])
-    markup = ReplyKeyboardMarkup(keyboard)
+    markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard = True)
     bot.send_message(chat_id=update.message.chat_id, text="Скинуть ещё?", reply_markup=markup)
-    if update.message.text == "Да":
-        send_gif(bot, update)
-    else:
-        bot.send_message(chat_id=update.message.chat_id, text='Окей, если захочешь повторить, то отправь мне /gif')
-
+    while update.message.text != "Да" or update.message.text != "Нет":
+        if update.message.text == "Да":
+            send_gif(bot, update)
+            break
+        else:
+            bot.send_message(chat_id=update.message.chat_id, text='Окей, если захочешь повторить, то отправь мне /gif')
+            break
 def startCommand(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Тебя приветствует PocketBuddy, твой карманный помошник и личный Telegram-проводник! \nОзнакомиться с доступными функциями ты сможешь, отправив /functions')
 def functionCommand(bot, update):
