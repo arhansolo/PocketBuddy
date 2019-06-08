f7969fbbf4783b15ed23d93897ba43264ba46364
arhansolo <arhansolo@yandex.ru>
2019-06-08 21:18:55 +0500

    talkfunc

--- __main__.py
+++ __main__.py
@@ -43,8 +43,6 @@ def startCommand(bot, update):
 def functionCommand(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Список функций: \n/gif - Команда, которая поднимет тебе настроение!")
 def talk(bot, update):
-    if update.message.text == "/stoptalk":
-        return
     def textMessage(bot, update):
         request = apiai.ApiAI(API_TOKEN).text_request()
         request.lang = 'ru'
@@ -58,10 +56,17 @@ def talk(bot, update):
         else:
             bot.send_message(chat_id=update.message.chat_id, text='Что ты сказал?')
 
+    def stoptalk(bot, update):
+        return True
+    if stoptalk(bot, update):
+        return
+
+
+    stoptalk_Command_handler = CommandHandler('stoptalk', stoptalk)
     text_message_handler = MessageHandler(Filters.text, textMessage)
     dispatcher.add_handler(text_message_handler)
+    dispatcher.add_handler(stoptalk_Command_handler)
     updater.start_polling(clean=True)
-
     updater.idle()
 
 function_Command_handler = CommandHandler('functions', functionCommand)
